302428827583e702f0bdda2ba2af901b3e1aabdf
Jose Seie <joseeugenioseie@hotmail.com>
2017-09-17 21:32:13 +0200

    Estudo de component iniciado

---
+++ src/Ninja.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+<h1>Lista de Ninjas</h1>
+  <ul>
+    <li v-for="ninja in ninjas">{{ninja}}</li> 
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: ['Hose','Jose Seie','HYSn']
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+    color: green;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import Ninjas from './Ninja.vue'
+// Vue.component('ninjas',Ninjas);
 
 new Vue({
   el: '#app',
