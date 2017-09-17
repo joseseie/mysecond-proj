3b9e42fa7acfad62b11f12f36c2a6236b30df112
Jose Seie <joseeugenioseie@hotmail.com>
2017-09-17 22:30:55 +0200

    Aplicacao Logica, funcionando bem
    Essa aplicacao feita na aula Vue JS 2 Tutorial 21

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-
   <div>
-      <h1>Meu primeiro texto</h1>
-      <ninja-seie></ninja-seie>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninja.vue'
+// import Ninjas from './Ninja.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
 
 export default {
   components:{
-    'ninja-seie': Ninjas
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas,
   },
   data () {
     return {
-      title: 'Second vue project'
+
     }
   },
   methods:{
 
   }
 }
-</script  scoped>
+</script>
 
-<style>
-  h1{
-    color: red;
-  }
+<style scoped>
+
 </style>
---
+++ src/components/Ninjas.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="ninjas">
+    <ul>
+    <li v-for="ninja in ninjas" v-on:click="ninja.show = !ninja.show">
+      <h2>{{ninja.name}}</h2>
+      <h3 v-show="ninja.show">{{ninja.speciality}}</h3>
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: [
+        {name: 'Hose',speciality: 'Jose Seie',show: false},
+        {name: 'Jose Seie',speciality: 'Teste Vuw',show: false},
+        {name: 'Mario Tanjo',speciality: 'Click Events',show: false},
+        {name: 'Tango',speciality: 'Conditionals',show: false},
+        {name: 'Kami',speciality: 'WebPack',show: false},
+        {name: 'Yoshi',speciality: 'Data Diggin',show: false},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#ninjas{
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+ul{
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li{
+  flex-grow: 1;
+  flex-basis: 300px;
+  padding: 30px;
+  text-align: center;
+  border: 1px solid #222;
+  margin: 10px;
+}
+</style>
