2a6af88b6c9bde55ae214b3d23309bcd0a8f90c2
Jose Seie <joseeugenioseie@hotmail.com>
2017-09-18 22:40:18 +0200

    Learnt v-bind with Select

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -14,9 +14,13 @@
         <label>Mario</label>
         <input type="checkbox" value="mario" v-model="blog.categories"/>
         <label>Cheese</label>
-        <input type="checkbox" value="cheese" v-model="blog.categories"/>
-
+        <input type="checkbox" value="cheese" v-model="blog.categories"/>  
       </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors">{{ author }}</option>
+      </select>  
+
     </form>
     <div id="preview">
       <h3>Preview Blog</h3>
@@ -26,6 +30,7 @@
       <ul>
       <li v-for="category in blog.categories">{{category}}</li>
       </ul>
+      <p>Author: {{blog.author}}</p>
     </div>
   </div>
 </template>
@@ -40,8 +45,10 @@ export default {
       blog: {
         title: '',
         content: '',
-        categories: []
-      }
+        categories: [],
+        author: ""
+      },
+      authors:['The Net Ninja','The angular avaneger','Terceiro Param']
     }
   },
   methods:{
